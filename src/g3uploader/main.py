db607a58516c45faa44008dcbcd60e2b2bf967b1
sharknasuhorse <nakaryo6459shark@gmail.com>
2019-10-14 14:03:30 +0900

    Add Class

--- src/g3uploader/main.py
+++ src/g3uploader/main.py
@@ -7,18 +7,28 @@ import os
 # flask run
 # set -x FLASK_ENV development
 
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+class G3uploader():
+
+    app = Flask(__name__)
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+    @app.route('/upload', methods=['GET', 'POST'])
+    def send():
+        if request.method == 'POST':
+            f = request.files['uploadFile']
+            f.save('./upload_data/' + secure_filename(f.filename))
+            app.logger.info('upload file saved')
+            return redirect(request.url)
+
+        return render_template('upload.html')
+
+    def run(self):
+        self.app.run(debug=True)
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def send():
-    if request.method == 'POST':
-        f = request.files['uploadFile']
-        f.save('./upload_data/' + secure_filename(f.filename))
-        app.logger.info('upload file saved')
-        return redirect(request.url)
-
-    return render_template('upload.html')
+if __name__ == "__main__":
+    web = G3uploader()
+    web.run(debug=True)
