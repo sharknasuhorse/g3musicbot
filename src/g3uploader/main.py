26d72e42d0ddd5a40fb3d1b9531adb3cecf0443f
sharknasuhorse <nakaryo6459shark@gmail.com>
2019-10-14 16:22:27 +0900

    Add convet and ouput json

--- src/g3uploader/main.py
+++ src/g3uploader/main.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, redirect, render_template
 from werkzeug.utils import secure_filename
-
+from pydub import AudioSegment
+import json
 import os
+from pprint import pprint
 
 # set -x FLASK_APP src/main.py
+# set -x FLASK_EsNV development
 # flask run
-# set -x FLASK_ENV development
 
 
 class G3uploader():
@@ -19,14 +21,85 @@ class G3uploader():
     def send():
         if request.method == 'POST':
             f = request.files['uploadFile']
-            f.save('./upload_data/' + secure_filename(f.filename))
-            app.logger.info('upload file saved')
+            filename = secure_filename(f.filename)
+            #dir = './upload_data/' + filename
+            dir = '../MusicBot/audio_cache/' + filename
+            f.save(dir)
+
+            # convert
+            song = AudioSegment.from_mp3(
+                dir)
+            duration = int('{:.0f}'.format(len(song) / 1000))
+            song.export(dir + '.webm', format='webm')
+
+            # output json
+            dir = dir + '.webm'
+            filename = filename + '.webm'
+            entry = URLPlaylistEntry(filename=filename, title=filename,
+                                     duration=duration, download_folder=dir)
+
+            with open('queue.json.temp', mode='r', encoding='utf8') as f:
+                queue_json = json.loads(f.read())
+
+            queue_json['data']['entries']['data']['entries'].append(
+                entry.__json__())
+
+            output_json_dir = '../MusicBot/data/525478015891931138/queue.json'
+            with open(output_json_dir, mode='w', encoding='utf8') as f:
+                f.write(json.dumps(queue_json))
+
             return redirect(request.url)
 
         return render_template('upload.html')
 
     def run(self, debug, host):
         self.app.run(debug=True, host=host)
+
+# Musicbot.entry
+
+
+class URLPlaylistEntry():
+    def __init__(self, filename, title, duration, download_folder):
+        self.filename = filename
+        self.title = title
+        self.duration = duration
+#        self.meta = meta
+        self.download_folder = download_folder
+
+    def _enclose_json(self, data):
+        return {
+            '__class__': self.__class__.__qualname__,
+            '__module__': 'musicbot.entry',
+            'data': data
+        }
+
+    def __json__(self):
+        return self._enclose_json({
+            'version': 1,
+            'url': 'g3uploader',
+            'title': self.title,
+            'duration': self.duration,
+            'downloaded': True,
+            'expected_filename': 'audio_cache/' + self.filename,
+            'filename': 'audio_cache/' + self.filename,
+            # 'full_filename': os.path.abspath(self.filename) if self.filename else self.filename,
+            'full_filename': '/Users/sharknasuhorse/Documents/git/g3musicbot/src/MusicBot/audio_cache/' + self.filename,
+            'meta': {
+                "author": {
+                    "id": 152646492593717250,
+                    "name": "sharknasuhorse",
+                    "type": "Member"
+                },
+                "channel": {
+                    "id": 632015617226047500,
+                    "name": "g3musicbot",
+                    "type": "TextChannel"
+                }
+            },
+            'aoptions': '-vn'
+        })
+
+
 def debug_json():
     with open('queue.json.temp', mode='r', encoding='utf8') as f:
         raw_json = f.read()
